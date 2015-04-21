4d524dd0d62da6bf25bfc262b6d51703b777caa1
Daniel <drx.patty@gmail.com>
2015-04-21 14:55:12 +0300

    fixed grunt copyjs

--- tasks/options/copy.js
+++ tasks/options/copy.js
@@ -5,14 +5,14 @@ module.exports = {
       {expand: true, flatten: true, src: ['src/bower_components/jquery/dist/*'], dest: 'demo/js/', filter: 'isFile'},
       {expand: true, flatten: true, src: ['src/bower_components/normalize.css/normalize.css'], dest: 'demo/css/', filter: 'isFile'},
       {expand: true, flatten: true, src: ['src/img/*'], dest: 'demo/img/', filter: 'isFile'},
-      {expand: true, flatten: true, src: ['src/app.js'], dest: 'demo/js/', filter: 'isFile'},
+      {expand: true, flatten: true, src: ['src/js/*'], dest: 'demo/js/', filter: 'isFile'},
       {expand: true, flatten: true, src: ['src/html/*'], dest: 'demo/', filter: 'isFile'}
     ]
   },
 
   watchJs: {
     files: [
-      {expand: true, flatten: true, src: ['src/js/app.js'], dest: 'demo/js/', filter: 'isFile'}
+      {expand: true, flatten: true, src: ['src/js/*'], dest: 'demo/js/', filter: 'isFile'}
     ]
   },
   
